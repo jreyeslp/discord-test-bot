07226fd4a656c5253cecd672b4878691963e50a0
Javi Reyes <jreyeslp@gmail.com>
2020-09-12 22:26:11 +0200

    New changes

--- events/guildMemberAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client, member) => {
-    member.send(`Bienvenido al servidor, ${member}! Las normas de clase son bien sencillitas, ya sabes ;)`);
-};
--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 require('dotenv').config()
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client();
 
-fs.readdir('./events/', (err, files) => {
-  files.forEach((file) => {
-    const eventHandler = require(`./events/${file}`);
-    const eventName = file.split('.')[0];
-    client.on(eventName, (...args) => eventHandler(client, ...args));
-  })
-});
+// Bot ready for kicking it!
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+// Te bajas dos tonitos
+client.on('message', (message) => {
+  if (message && message.content.includes("!tonitos") > 0) {
+    const number = message.content.split(' ')[0];
+    const member = message.mentions.members.first()
+    if (!member) {
+      return message.reply(`A quién increpo, mongolín?`)
+    }
+    return message.channel.send(`${member}, te bajas ${number} tonitos crack`);
+  }
+})
+
+// Send a remote file
+client.on('message', (message) => {
+  if(message && message.content.startsWith("!pmilos")) {
+    message.channel.send({
+      files: ['https://cdn.discordapp.com/attachments/440900194414886925/754375865034604684/pm.gif']
+    })
+      .then(console.log)
+      .catch(console.error);
+  }
+})
 
 client.login(process.env.BOT_TOKEN);
